469366a5b775e7124b26891cf0f7ab7528e27c46
sony@prophecy.io <sony@prophecy.io>
2024-03-26 09:21:47 +0000

    ❇️ New     - Pipeline        - nom

---
+++ python/new1/pipelines/nom/code/p0o/pipeline.py
@@ -0,0 +1,25 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from p0o.config.ConfigStore import *
+from p0o.udfs.UDFs import *
+from prophecy.utils import *
+
+def pipeline(spark: SparkSession) -> None:
+    pass
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/nom")
+    registerUDFs(spark)
+    
+    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/nom", config = Config)(pipeline)
+
+if __name__ == "__main__":
+    main()
---
+++ python/new1/pipelines/nom/code/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'nom',
+    version = '1.0',
+    packages = find_packages(include = ('p0o*', )) + ['prophecy_config_instances'],
+    package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
+    package_data = {'prophecy_config_instances' : ['*.json', '*.py', '*.conf']},
+    description = 'workflow',
+    install_requires = [
+'prophecy-libs==1.8.12'],
+    entry_points = {
+'console_scripts' : [
+'main = p0o.pipeline:main'], },
+    data_files = [(".prophecy", [".prophecy/workflow.latest.json"])],
+    extras_require = {
+'test' : ['pytest', 'pytest-html'], }
+)
